1e16402171c4395da0fa4f7ea2ae906f2853d595
fraser <ch.p.fraser@gmail.com>
2017-03-31 10:30:17 +0200

    changed the value of api to lowercase

--- php/api.php
+++ php/api.php
@@ -1,9 +1,10 @@
 <?php
 if(isset($_POST) && !empty($_POST)){
+echo $value;
 	foreach($_POST as $key => $value){	
 		switch($key){
 			case "window":
-				$result = file_get_contents(".load/" . $value . ".php");
+				$result = file_get_contents(".load/" . strtolower($value) . ".php");
 				break;
 		}
 	}
