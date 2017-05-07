25cef53ef30e4688acceb1a9b5cc46f05095c6ad
Michi Bötschi <Michi Btschi>
2017-05-07 15:58:46 +0200

    updated image display structure

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ $(".redirect").on("click", function( e ){
 					createCookie('videoPointer', 0);
 					var jsData = $.parseJSON( getCookie('videoData') );
 					var videoCounter = getCookie('videoPointer');
-					replaceBody(data, jsData, videoCounter);
+					replaceBodyVideos(data, jsData, videoCounter);
 				});
 				break;
 			case "Images":
@@ -27,21 +27,33 @@ $(".redirect").on("click", function( e ){
 					data: { "getter":  "images" }
 				}).done(function( videoData ){
 					var jsData = $.parseJSON( videoData );
-					var videoCounter = 0;
-					replaceBody(data, jsData, videoCounter);
+					var imageCounter = jsData.length;
+					replaceBodyImages(data, jsData, imageCounter);
 				});
 				break;
 		}
 	});
 })
 
-function replaceBody(data, jsData, videoCounter){
-	console.log( data );
-	console.log( jsData );
-	console.log( videoCounter );
+function replaceBodyVideos(data, jsData, videoCounter){
 	data = data.replace("PATH", jsData[ videoCounter ].path);
 	data = data.replace("DATATYPE", jsData[ videoCounter ].datatype);
 	data = data.replace("TITLE", jsData[ videoCounter ].name);
 	data = data.replace("DESCRIPTION", jsData[ videoCounter ].description);
 	$(".main").replaceWith(data);
+}
+
+function replaceBodyImages(data, jsData, imgCounter){
+	var result = "<div class='main wrapper clearfix'><div class='gallery cf'>";
+	var arg = "";
+	for(i = 0; i < imgCounter; i++){
+		arg = data;
+		arg = arg.replace("PATH", jsData[ i ].path);
+		arg = arg.replace("TITLE", jsData[ i ].name);
+		arg = arg.replace("DESCRIPTION", jsData[ i ].description);
+		result += arg;
+		arg = "";
+	}
+	result += "</div></div>";
+	$(".main").replaceWith(result);
 }
--- php/.load/images.php
+++ php/.load/images.php
@@ -1,38 +1,5 @@
-<div class="main wrapper clearfix">
-	<div class="gallery cf">
-			<div>
-			<h4>TITLE</h4>
-			<img src=PATH />		
-			<p>DESCRIPTION</p>
-		</div>
-			<div>
-			<h4>TITLE</h4>
-			<img src="http://abload.de/img/a6aawu.jpg" />		
-			<p>DESCRIPTION</p>
-		</div>
-		<div>
-			<h4>TITLE</h4>
-			<img src="http://abload.de/img/a6aawu.jpg" />		
-			<p>DESCRIPTION</p>
-		</div>
-			<div>
-			<h4>TITLE</h4>
-			<img src="http://abload.de/img/a6aawu.jpg" />		
-			<p>DESCRIPTION</p>
-		</div>
-			<div>
-			<h4>TITLE</h4>
-			<img src="http://abload.de/img/a6aawu.jpg" />		
-			<p>DESCRIPTION</p>
-		</div>
-			<div>
-			<h4>TITLE</h4>
-			<img src="http://abload.de/img/a6aawu.jpg" />		
-			<p>DESCRIPTION</p>
-		</div>
-
-
-	</div>
-
+<div>
+	<h4> TITLE </h4>
+	<img src="PATH" />		
+	<p> DESCRIPTION </p>
 </div>
-
